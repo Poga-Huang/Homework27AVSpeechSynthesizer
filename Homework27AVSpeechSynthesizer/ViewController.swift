83c412110e92cf8f1827e39f5b99d9f68c11647b
Poga Huang <pgh86812@gmail.com>
2021-11-30 16:21:37 +0800

    Initial Commit

---
+++ Homework27AVSpeechSynthesizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework27AVSpeechSynthesizer
+//
+//  Created by 黃柏嘉 on 2021/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
